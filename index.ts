4cb3c44acbec070c8598cfd1a4c64c857c10c3e1
EnzoRobaina <enzo.robaina@gmail.com>
2022-10-16 22:18:32 -0300

    Adds basic client and prints qr to stdout

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log('hi')
+import {Client, LocalAuth} from 'whatsapp-web.js'
+import qrcode from 'qrcode-terminal'
+
+const client = new Client({authStrategy: new LocalAuth()})
+
+client.on('qr', qr => {
+  console.log(`got qr at ${new Date().toISOString()}`)
+  qrcode.generate(qr, {small: true})
+})
+
+client.on('ready', () => {
+  console.log('Client is ready!')
+})
+
+client.on('message', msg => {
+  console.log(msg)
+})
+
+client.initialize()
